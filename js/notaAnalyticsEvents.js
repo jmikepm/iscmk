91670df74af865597e16fae45c97d1393982feaa
jmikepm <jmiguelpm14@gmail.com>
2019-05-29 16:26:42 -0500

    Update notaAnalyticsEvents.js

--- js/notaAnalyticsEvents.js
+++ js/notaAnalyticsEvents.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	$('.lnkbtndw').on('click', function() {
-	    ga('send', 'event', 'DownloadCV', 'Header', {'page': '/iscmk/home'});
+	    ga('send', 'event', 'DownloadCV', 'click', {'page': '/iscmk/home'});
 	});
 
 
