cd34d47e5e15a983d5cb27a8241476a934d875d0
miguel <miguel@troquer.com.mx>
2021-03-31 10:40:23 -0600

    Analityc

--- js/notaAnalyticsEvents.js
+++ js/notaAnalyticsEvents.js
@@ -1,14 +1,3 @@
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139716800-1"></script>
-
-		window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'UA-139716800-1');
-
-
-
 $(document).ready(function() {
 
 	$('.lnkbtndw').on('click', function() {
